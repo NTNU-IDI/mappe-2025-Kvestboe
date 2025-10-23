52cc90a53288d202e41ed6d22c1ffdc6ed267495
Kristian Vestbø <vestboe.kristian@gmail.com>
2025-10-23 14:40:57 +0200

    testing commit

--- src/main/java/edu/ntnu/iir/bidata/Main.java
+++ src/main/java/edu/ntnu/iir/bidata/Main.java
@@ -1,10 +1,13 @@
+import java.util.Scanner;
 
 public class Main {
 
 
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
 
 		System.out.println("Dette er den første testen med å lage en fil i nvim");
+		System.out.println(input.nextLine());
 
 	}
 
