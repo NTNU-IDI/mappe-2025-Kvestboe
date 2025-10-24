9e55d0f4f3693737005a446147cdc60ea9076744
Kristian Vestbø <vestboe.kristian@gmail.com>
2025-10-24 20:51:23 +0200

    add maven, add User, add Test

--- src/main/java/edu/ntnu/iir/bidata/home/User.java
+++ src/main/java/edu/ntnu/iir/bidata/home/User.java
@@ -1,7 +1,27 @@
-package edu.ntnu.iir.bidata.Home;
+package edu.ntnu.iir.bidata.home;
 
 
 public class User {
     
+    private String name;
     
+    public User(String inputName) {
+        name = inputName;
+    }
+    
+
+    public boolean checkUser(String inputName) {
+        return name.equals(inputName);
+    }
+    
+    // might want to personalize the experience a little, and add a password for safety
+    // private String password;
+    // private String favoriteFood;
+    // 
+    // public User(String inputName, String inputPassword, String inputFavoriteFood) {
+    //     name = inputName;
+    //     password = inputPassword;
+    //     favoriteFood = inputFavoriteFood;
+    // }
+   
 }
