ed5ea47cd60df600a2f38145e832293ed4ca2348
Kristian Vestbø <vestboe.kristian@gmail.com>
2025-11-21 12:48:29 +0100

    remove the settings classes

--- src/main/java/edu/ntnu/iir/bidata/model/Settings.java
+++
@@ -1,8 +0,0 @@
-package edu.ntnu.iir.bidata.model;
-
-public class Settings {
-    // handles the settings of the user, so that every user can have a different configuration
-    // communicates with the settings.json file
-    // loads the settings upon start
-    // password encryption if included in program
-}
--- src/main/java/edu/ntnu/iir/bidata/storage/SettingsManager.java
+++
@@ -1,7 +0,0 @@
-package edu.ntnu.iir.bidata.storage;
-
-public class SettingsManager {
-    // will read the config the user has saved
-    // save updated settings
-    // provide default if no settings applied
-}
