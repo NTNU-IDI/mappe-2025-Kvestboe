629d20d1b7712f4bcdf7b5380087493805fcf714
Kristian <vestboe.kristian@gmail.com>
2025-12-05 18:15:04 +0100

    Add AuthorManagerTest.

--- src/test/java/storageTest/AuthorManagerTest.java
+++ src/test/java/storageTest/AuthorManagerTest.java
@@ -1,5 +1,66 @@
 package storageTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import edu.ntnu.iir.bidata.storage.AuthorManager;
+import java.util.ArrayList;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class AuthorManagerTest {
+  AuthorManager authorManager;
+
+  @BeforeEach
+  void setUp() {
+    authorManager = new AuthorManager();
+
+  }
+
+  @Test
+  void authorManagerAddAuthorTest() {
+    ArrayList<String> authors = new ArrayList<>();
+    authors.add("Kristian");
+
+    authorManager.addAuthor("Kristian");
+    assertEquals(
+        "Kristian",
+        authorManager.getAuthor("Kristian").getName(),
+        "AuthorManager should return author with name Kristian"
+    );
+    assertEquals(
+        authors,
+        authorManager.getAuthors(),
+        "AuthorManager should return ArrayList of one string with value Kristian"
+    );
+
+  }
+
+  @Test
+  void authorManagerGettersTest() {
+    ArrayList<String> authors = new ArrayList<>();
+    authors.add("Kristian");
+    authors.add("Elon Musk");
+    authors.add("Obama");
+
+    authorManager.addAuthor("Kristian");
+    authorManager.addAuthor("Elon Musk");
+
+    authorManager.addAuthor("Kristian");
+    assertEquals(
+        "Kristian",
+        authorManager.getAuthor("Kristian").getName(),
+        "AuthorManager should return author with name Kristian"
+    );
+
+    assertEquals(
+        authors,
+        authorManager.getAuthors(),
+        "AuthorManager should return ArrayList of one string with same value "
+            + "Kristian, Elon Musk and Obama"
+    );
+
+  }
+
+
 
 }
